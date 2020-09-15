c79e68fba3b1ebd828770af20cc5a7a81e8eb8f6
Tingzi123 <1044373556@qq.com>
2020-09-15 10:11:50 +0800

    [chenting] fix:add salesTax and tot for Order class

--- 1/src/main/java/com/twu/refactoring/Order.java
+++ 1/src/main/java/com/twu/refactoring/Order.java
@@ -5,12 +5,16 @@ import java.util.List;
 public class Order {
     String customerName;
     String customerAddress;
+    double totSalesTx;
+    double tot;
     List<LineItem> lineItems;
 
     public Order(String customerName, String customerAddress, List<LineItem> lineItems) {
         this.customerName = customerName;
         this.customerAddress = customerAddress;
         this.lineItems = lineItems;
+        totSalesTx = 0d;
+        tot = 0d;
     }
 
     public String getCustomerName() {
@@ -23,5 +27,21 @@ public class Order {
 
     public List<LineItem> getLineItems() {
         return lineItems;
+    }
+
+    public double getTotSalesTx() {
+        return totSalesTx;
+    }
+
+    public void setTotSalesTx(double totSalesTx) {
+        this.totSalesTx = totSalesTx;
+    }
+
+    public double getTot() {
+        return tot;
+    }
+
+    public void setTot(double tot) {
+        this.tot = tot;
     }
 }
--- 1/src/main/java/com/twu/refactoring/OrderReceipt.java
+++ 1/src/main/java/com/twu/refactoring/OrderReceipt.java
@@ -5,51 +5,51 @@ public class OrderReceipt {
 
     public OrderReceipt(Order order) {
         this.order = order;
-	}
+    }
 
-	public String printReceipt() {
-		StringBuilder output = new StringBuilder();
+    public String printReceipt() {
+        StringBuilder output = new StringBuilder();
 
-		final String header="======Printing Orders======\n";
-		output.append(header);
+        final String header = "======Printing Orders======\n";
+        output.append(header);
 
         output.append(order.getCustomerName());
         output.append(order.getCustomerAddress());
 
-		printsLineItems(output);
-		return output.toString();
-	}
+        printsLineItems(output,order);
+        return output.toString();
+    }
 
-	private void printsLineItems(StringBuilder output) {
-		double totSalesTx = 0d;
-		double tot = 0d;
-		for (LineItem lineItem : order.getLineItems()) {
-			traversalPrintLineItems(output, lineItem);
+    private void printsLineItems(StringBuilder output,Order order) {
+        double totSalesTx = order.getTotSalesTx();
+        double tot = order.getTot();
+        for (LineItem lineItem : order.getLineItems()) {
+            traversalPrintLineItems(output, lineItem);
 
-			// calculate sales tax @ rate of 10%
-			final double taxRate = .10;
-			double salesTax = lineItem.totalAmount() * taxRate;
+            // calculate sales tax @ rate of 10%
+            final double taxRate = .10;
+            double salesTax = lineItem.totalAmount() * taxRate;
             totSalesTx += salesTax;
 
             // calculate total amount of lineItem = price * quantity + 10 % sales tax
             tot += lineItem.totalAmount() + salesTax;
-		}
+        }
 
-		// prints the state tax
-		output.append("Sales Tax").append('\t').append(totSalesTx);
+        // prints the state tax
+        output.append("Sales Tax").append('\t').append(totSalesTx);
 
-		// print total amount
-		output.append("Total Amount").append('\t').append(tot);
-	}
+        // print total amount
+        output.append("Total Amount").append('\t').append(tot);
+    }
 
-	private void traversalPrintLineItems(StringBuilder output, LineItem lineItem) {
-		output.append(lineItem.getDescription());
-		output.append('\t');
-		output.append(lineItem.getPrice());
-		output.append('\t');
-		output.append(lineItem.getQuantity());
-		output.append('\t');
-		output.append(lineItem.totalAmount());
-		output.append('\n');
-	}
+    private void traversalPrintLineItems(StringBuilder output, LineItem lineItem) {
+        output.append(lineItem.getDescription());
+        output.append('\t');
+        output.append(lineItem.getPrice());
+        output.append('\t');
+        output.append(lineItem.getQuantity());
+        output.append('\t');
+        output.append(lineItem.totalAmount());
+        output.append('\n');
+    }
 }
